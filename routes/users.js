4e803ca765dcc2bbdd248c5136378969806bf047
diya goyal <diya.goyal1607@gmail.com>
2026-03-31 12:54:22 +0530

    crud done

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,60 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User.js')
 
-router.get('/', (req, res) => {
-  res.json([
-    { id: 1, name: "ab" },
-    { id: 2, name: "cd" }
-  ])
+ 
+
+router.get('/', async (req, res) => { // find user
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+})
+ 
+
+router.delete('/:id' , async (req, res) => { // delete user
+  try{
+    const user =  await User.findByIdAndDelete(req.params.id)
+    console.log("deleted" , user)
+    res.json({ message: "User deleted!", user })
+  }catch (err) {
+    res.status(400).json({error: err.message})
+  }
 })
 
 
-router.get('/search', (req, res) => {
-  console.log(req.query)
-  res.json(req.query)
-}) 
+router.put('/:id' , async (req, res) => { // update user
+  try{
+    const user =  await User.findByIdAndUpdate(req.params.id , req.body  , { new: true })
+    
+    console.log("Updated" , user)
+    res.json({ message: "User updated!", user })
+  }catch (err) {
+    res.status(400).json({error: err.message})
+  }
+})
+
+
+
+
+
+router.post('/', async (req, res) => { // save user
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    await user.save()
+    res.status(201).json({ message: "User saved!", user })
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+})
+
+
 
 
 module.exports = router
