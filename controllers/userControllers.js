8af454bc57766188fcf2888010314648365fe429
diya goyal <diya.goyal1607@gmail.com>
2026-04-04 22:22:57 +0530

    mvc

---
+++ controllers/userControllers.js
@@ -0,0 +1,57 @@
+ 
+const User = require('../models/User.js')
+const bcrypt = require('bcrypt')
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+}
+
+
+ const saveusers = async (req, res) => { // save user
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword 
+    })
+    
+    await user.save()
+    res.status(201).json({ message: "User saved!", user })
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+}
+
+ 
+ 
+
+const deleteUser =  async (req, res) => { // delete user
+  try{
+    const user =  await User.findByIdAndDelete(req.params.id)
+    console.log("deleted" , user)
+    res.json({ message: "User deleted!", user })
+  }catch (err) {
+    res.status(400).json({error: err.message})
+  }
+}
+
+
+ const updateUser =  async (req, res) => { // update user
+  try{
+    const user =  await User.findByIdAndUpdate(req.params.id , req.body  , { new: true })
+    
+    console.log("Updated" , user)
+    res.json({ message: "User updated!", user })
+  }catch (err) {
+    res.status(400).json({error: err.message})
+  }
+}
+
+module.exports = { getAllUsers  , updateUser , deleteUser , saveusers}
--- routes/users.js
+++ routes/users.js
@@ -1,68 +1,11 @@
-const express = require('express')
+ const express = require('express')
 const router = express.Router()
-const User = require('../models/User.js')
 const authMiddleware = require('../middleware/authMiddleware')
+const { getAllUsers, saveusers, updateUser, deleteUser } = require('../controllers/userController')
 
-const bcrypt = require('bcrypt')
- 
-
-
-router.post('/', async (req, res) => { // save user
-  const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-  try {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword 
-    })
-    
-    await user.save()
-    res.status(201).json({ message: "User saved!", user })
-  } catch (err) {
-    res.status(400).json({ error: err.message })
-  }
-})
-
-router.get('/',authMiddleware ,async (req, res) => { // find user
-  try {
-    const users = await User.find()
-    res.json(users)
-  } catch (err) {
-    res.status(400).json({ error: err.message })
-  }
-})
- 
-
-router.delete('/:id' , async (req, res) => { // delete user
-  try{
-    const user =  await User.findByIdAndDelete(req.params.id)
-    console.log("deleted" , user)
-    res.json({ message: "User deleted!", user })
-  }catch (err) {
-    res.status(400).json({error: err.message})
-  }
-})
-
-
-router.put('/:id' , async (req, res) => { // update user
-  try{
-    const user =  await User.findByIdAndUpdate(req.params.id , req.body  , { new: true })
-    
-    console.log("Updated" , user)
-    res.json({ message: "User updated!", user })
-  }catch (err) {
-    res.status(400).json({error: err.message})
-  }
-})
-
-
-// tokens
-
-
- 
-
-
-
+router.get('/', authMiddleware, getAllUsers)
+router.post('/', saveusers)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 module.exports = router
